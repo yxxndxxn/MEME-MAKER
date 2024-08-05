07d7a836088d7f41696d5833c7ffec077d4dd1c2
yxxndxxn <yxxndxxn@gmail.com>
2024-08-05 16:52:50 +0900

    style: example(making face)

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");  //getContext는 붓이라고 생각하면 됨..!
-canvas.width =800;
-canvas.height =800;
+canvas.width = 800;
+canvas.height = 800;
 
-ctx.moveTo(50, 50);  //브러쉬의 좌표를 움직여줌
-ctx.lineTo(150,50);  //라인을 그려줌
-ctx.lineTo(150,150);
-ctx.lineTo(50,150);
-ctx.lineTo(50,50);
-ctx.fillStyle ="green";
+ctx.arc(200, 200, 50, 0, 2*Math.PI); //얼굴
+ctx.fillStyle = "green";
 ctx.fill();
 
-ctx.beginPath();  //새 경로 시작
-ctx.fillStyle ="skyBlue";
-ctx.fillRect(200, 300, 40, 200);  //왼쪽 벽
-ctx.fillRect(400, 300, 40, 200);  //오른쪽 벽
-ctx.fillRect(200, 300, 200, 20);  //윗쪽 벽
-ctx.fillRect(200, 500, 240, 20);  //아랫쪽 벽
-ctx.fillRect(300, 400, 40, 100);  //문
+//눈
+ctx.beginPath();
+ctx.arc(188, 195, 5, 0, 2*Math.PI);  
+ctx.arc(212, 195, 5, 0, 2*Math.PI);
+ctx.fillStyle = "white";
+ctx.fill();
 
-ctx.moveTo(200, 300);  //지붕
-ctx.lineTo(320, 200);
-ctx.lineTo(440, 300);
-
+//입
+ctx.beginPath();
+ctx.arc(200, 208, 24, 0, 1*Math.PI);
+ctx.fillStyle = "orange";
 ctx.fill();
