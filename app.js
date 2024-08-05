5351f52b3479295ed1c3bb450a7f0ba92e09a41a
yxxndxxn <yxxndxxn@gmail.com>
2024-08-05 16:09:35 +0900

    style: example(moveTo(),lineTo() -> making house)

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const ctx = canvas.getContext("2d");  //getContext는 붓이라고 생각하면 
 canvas.width =800;
 canvas.height =800;
 
-//경로 1 (와 약간 아텍이네 ㄷㄷ!!)
-ctx.rect(50, 50, 100, 100);
-ctx.fillStyle ="skyBlue";
+ctx.moveTo(50, 50);  //브러쉬의 좌표를 움직여줌
+ctx.lineTo(150,50);  //라인을 그려줌
+ctx.lineTo(150,150);
+ctx.lineTo(50,150);
+ctx.lineTo(50,50);
+ctx.fillStyle ="green";
 ctx.fill();
 
-ctx.beginPath();  //새 경로 시작하기: beginPath()
+ctx.beginPath();  //새 경로 시작
+ctx.fillStyle ="skyBlue";
+ctx.fillRect(200, 300, 40, 200);  //왼쪽 벽
+ctx.fillRect(400, 300, 40, 200);  //오른쪽 벽
+ctx.fillRect(200, 300, 200, 20);  //윗쪽 벽
+ctx.fillRect(200, 500, 240, 20);  //아랫쪽 벽
+ctx.fillRect(300, 400, 40, 100);  //문
 
-//경로 2
-ctx.rect(150, 150, 100, 100);
-ctx.fillStyle ="green";
+ctx.moveTo(200, 300);  //지붕
+ctx.lineTo(320, 200);
+ctx.lineTo(440, 300);
+
 ctx.fill();
